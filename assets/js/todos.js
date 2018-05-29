e939db3e92f939594c7f4c00689b0621cf338de2
Zokkydev <chiazokamecheta@gmail.com>
2018-05-29 10:21:43 +0100

    Modified To Do App (Partial)

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -8,34 +8,43 @@ $("ul").on("click", "li", function(){
 
 	if($(this).css("color") === "rgb(128, 128, 128)"){
 
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
+		$(this).css("color", "black");
 
 	}
 
 	else{
 
-		$(this).css( {
-			color: "gray",
-			textDecoration: "line-through"
-		});
+		$(this).css("color", "gray");
 
 	}
 
 })
 
 //Click on X to delete
+//.fa-trash-alt specifies that it's the span with "fa-trash-alt" class that is to be deleted
 
-$("ul").on("click", "span", function(event){
+$("ul").on("click", ".fa-trash-alt", function(event){
 
 	$(this).parent().fadeOut(500, function(){
 
 		$(this).remove();
 	});
 
-	event.stopPropagation();
+	event.stopPropagation();              //Stops the parent from executing the code
+});
+
+
+
+//Check to take item to completed tasks
+
+$("ul").on("click", "#check", function(event){
+
+	$(this).parent().fadeOut(500, function(){
+
+		$(this).remove();
+	});
+
+	event.stopPropagation();              //Stops the parent from executing the code
 });
 
 //Capturing the user's input
@@ -43,7 +52,7 @@ $("input").on("keypress", function(event){
 
 	if(event.which === 13){
 
-		$("ul").append("<li><span><i class='fa fa-trash-alt'></i></span> " + $(this).val() + "</li>");
+		$("ul").append("<li><span id ='check'><input type='checkbox'><span><i class='fa fa-trash-alt'></i></span> " + "&nbsp; &nbsp; &nbsp" + $(this).val() + "</li>");
 
 		$("input").val("");
 	}
